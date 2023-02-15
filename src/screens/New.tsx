99f1099a4c5a403685664949b89667ffc645869c
Samuel Cabral <samuelcabral.mail@gmail.com>
2023-02-15 00:04:06 -0300

    Criar novo hábito

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,6 +1,7 @@
 import { Feather } from '@expo/vector-icons'
 import { useState } from 'react'
 import {
+  Alert,
   ScrollView,
   Text,
   TextInput,
@@ -10,6 +11,7 @@ import {
 import colors from 'tailwindcss/colors'
 import { BackButton } from '../components/BackButton'
 import { Checkbox } from '../components/Checkbox'
+import { api } from '../lib/axios'
 
 const availableWeekDays = [
   'Domingo',
@@ -22,6 +24,7 @@ const availableWeekDays = [
 ]
 
 export function New() {
+  const [title, setTitle] = useState('')
   const [weekDays, setWeekDays] = useState<number[]>([])
 
   function handleToggleWeekDay(weekDayIndex: number) {
@@ -29,6 +32,27 @@ export function New() {
       setWeekDays(weekDays.filter((item) => item !== weekDayIndex))
     } else {
       setWeekDays([...weekDays, weekDayIndex])
+    }
+  }
+
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert(
+          'Novo hábito',
+          'Informe o nome do hábito e escolha a recorrência',
+        )
+      }
+
+      await api.post('habits', { title, weekDays })
+
+      setTitle('')
+      setWeekDays([])
+
+      Alert.alert('Novo hábito', 'Hábito criado com sucesso!')
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Ops', 'Não foi possível criar o novo hábito')
     }
   }
 
@@ -48,6 +72,8 @@ export function New() {
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
           placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[600]}
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Text className="text-white mt-5 mb-3 font-semibold text-base">
@@ -65,7 +91,11 @@ export function New() {
           )
         })}
 
-        <TouchableOpacity className="w-full h-14 mt-6 flex-row items-center bg-green-500 rounded-lg justify-center">
+        <TouchableOpacity
+          className="w-full h-14 mt-6 flex-row items-center bg-green-500 rounded-lg justify-center"
+          activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
+        >
           <Feather name="check" size={20} color={colors.white} />
           <Text className="ml-2 font-semibold text-base text-white">
             Confirmar
